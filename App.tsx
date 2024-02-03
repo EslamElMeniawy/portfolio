c5e9baaea55d5a987cb3484831f4d3c488ba4fe2
Eslam El Meniawy <eslam.elmeniawy@gmail.com>
2024-02-03 17:18:40 +0200

    chore: prebuild

--- App.tsx
+++ App.tsx
@@ -4,8 +4,6 @@ import * as SplashScreen from "expo-splash-screen";
 import * as React from "react";
 import { enableScreens } from "react-native-screens";
 
-import * as serviceWorkerRegistration from "./src/service-worker/serviceWorkerRegistration";
-
 enableScreens();
 
 SplashScreen.preventAutoHideAsync();
@@ -13,8 +11,3 @@ SplashScreen.preventAutoHideAsync();
 export default function App() {
   return <MainEntry />;
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
